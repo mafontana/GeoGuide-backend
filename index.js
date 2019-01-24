009a5aa43af17a3f814d70ca2c5408af335f322d
Meredith Fontana <meredith@Merediths-MacBook-Pro.local>
2019-01-24 15:56:44 -0700

    created a route folder and a get route. also installed body-parser

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const port = process.env.PORT || 4010
 const app = express()
+const bodyParser = require('body-parser')
 
-app.get('/', (req, res) => {
-    res.send("hello world")
-    
-})
+app.use(bodyParser.json())
+
+const routes = require('./routes/api')
+
+app.use(routes)
+
 
 app.listen(port, () => console.log(`listening on port ${port}`))
--- routes/api.js
+++ routes/api.js
@@ -1,2 +1,8 @@
 const express = require('express');
 const router = express.Router()
+
+router.get('/points', function(req, res) {
+    res.send({type: 'GET'})
+})
+
+module.exports = router 
