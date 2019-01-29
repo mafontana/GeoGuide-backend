341fa2472a545da4aa7d9d985b10d287bef89939
Meredith Fontana <meredith@Merediths-MacBook-Pro.local>
2019-01-28 17:55:47 -0700

    created more detailed schema for geologic data

--- index.js
+++ index.js
@@ -12,13 +12,8 @@ app.use(bodyParser.json())
 
 app.use('/api', require('./routes/api'));
 
-
-
 app.use(function(err, req, res, next) {
     res.status(422).send({error: err.message})
 })
-
-
-
 
 app.listen(port, () => console.log(`listening on port ${port}`))
--- models/POIs.js
+++ models/POIs.js
@@ -1,38 +1,82 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
-//create geolocation Schema
+//geolocation Schema
 
-const GeoSchema = new Schema({
-    type: {
-        type: String,
-        default: "Point"
-    },
-    coordinates: {
-        type: [Number],
-        index: "2dsphere"
-    }
+const geologic_formation_schema = new Schema({
+   formation_1: {
+    name: String,
+    age_mya: Number,
+    geologic_age: String,
+    description: String
+   },
+   formation_2: {
+    name: String,
+    age_mya: Number,
+    description: String
+   },
+   formation_3: {
+    name: String,
+    age_mya: Number,
+    description: String
+   }
 })
 
-const GeoSchemaTrail = new Schema({
+const AR_scene_coordinate_schema = new Schema({
     type: {
         type: String,
-        default: "LineString"
+        default: "MultiPoint"
     },
-    coordinates: {
+    coordinates_AR_scene1: {
         type: [Number],
         index: "2dsphere"
-    }
+    }, 
+    coordinates_AR_scene2: {
+        type: [Number],
+        index: "2dsphere"
+    },
+    coordinates_AR_scene3: {
+        type: [Number],
+        index: "2dsphere"
+    }, 
+    coordinates_AR_scene4: {
+        type: [Number],
+        index: "2dsphere"
+    },
+    coordinates_AR_scene5: {
+        type: [Number],
+        index: "2dsphere"
+    }, 
+    coordinates_AR_scene6: {
+        type: [Number],
+        index: "2dsphere"
+    },
+    coordinates_AR_scene7: {
+        type: [Number],
+        index: "2dsphere"
+    },
+    coordinates_AR_scene8: {
+        type: [Number],
+        index: "2dsphere"
+    },
+    coordinates_AR_scene9: {
+        type: [Number],
+        index: "2dsphere"
+    },
+    coordinates_AR_scene10: {
+        type: [Number],
+        index: "2dsphere"
+    },
 })
-
 
 
 
 const POIschema = new Schema({
     name: {
         type: String
-    },
-    geometry: GeoSchema
+    }, 
+    AR_scenes: AR_scene_coordinate_schema,
+    geologic_formations: geologic_formation_schema, 
 })
 
 const POI = mongoose.model('POI', POIschema)
